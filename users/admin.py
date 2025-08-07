ab4e46ac9e2e4aa760538b0a043724da5093088f
raahulydv7 <rahul7879yadav@gmail.com>
2025-08-08 00:39:23 +0530

    modified signals & admin view

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import CustomUser
+from .models import CustomUser,UserProfile
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ['id', 'username', 'email', 'role', 'created_at']
     search_fields = ['id', 'username', 'email']
+    ordering = ['-created_at']
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'fname','lname' 'created_at']
+    search_fields = ['id', 'user__username','fname','lname']
     ordering = ['-created_at']
